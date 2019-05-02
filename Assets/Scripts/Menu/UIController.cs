72feb130135d359d1c6917f92687585da2de2fc2
miguelangelmadrid <m.angelmadrid@gmail.com>
2019-05-02 20:16:56 +0200

    Programacion basica de interfaz

---
+++ Assets/Scripts/Menu/UIController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] GameObject main, options;
+    [SerializeField] AudioMixer master;
+    [SerializeField] Dropdown graphicsSel;
+    [SerializeField] Dropdown resolutionSel;
+    List<string> myList = new List<string>();
+
+    Resolution[] myRes;
+
+    private void Start()
+    {
+        myRes = Screen.resolutions;
+
+        foreach (Resolution res in myRes)
+        {
+            myList.Add(res.ToString());
+        }
+        resolutionSel.AddOptions(myList);
+    }
+
+    public void Options()
+    {
+        main.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void BackMain()
+    {
+        main.SetActive(true);
+        options.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+
+    //--------------------------------
+    //AUDIO
+    public void ControlVolume(float volume)
+    {
+        master.SetFloat("Volume", volume);
+    }
+
+    //--------------------------------
+    //Graphics
+
+    public void SetGraphics(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+        graphicsSel.value = index;
+        graphicsSel.RefreshShownValue();
+    }
+
+    public void CheckScreen(bool check)
+    {
+        Screen.fullScreen = check;
+
+    }
+    public void SetResolution(int index)
+    {
+        Screen.SetResolution(myRes[index].width, myRes[index].height, Screen.fullScreen);
+        resolutionSel.value = index;
+        resolutionSel.RefreshShownValue();
+    }
+
+
+}
